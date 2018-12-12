be54088dca9fdb87171ee271261e5c1a0c62d71f
Tim Wilkens <timwilkensf1@gmail.com>
2018-12-12 07:33:25 -0800

    Much nicer dynamic programming solution to Challenge 11 Part 2

--- 11/part2.py
+++ 11/part2.py
@@ -7,49 +7,52 @@ def solve():
   total_power_cache = {}
 
   # Build the grid with power levels for each cell
+  # Base case: square of size 1
   grid = {}
-  x = 1
-  while x <= 300:
-    y = 1
-    while y <= 300:
-      grid[str(x) + "," + str(y)] = calculuate_power_level(x, y, grid_serial_number)
-      total_power_cache[str(x) + "," + str(y) + ",1"] = calculuate_power_level(x, y, grid_serial_number)
-      y = y + 1
-    x = x + 1
+  for x in range (1, 301):
+    total_power_cache[x] = {}
+    grid[x] = {}
+    for y in range(1, 301):
+      total_power_cache[x][y] = {}
+      grid[x][y] = calculuate_power_level(x, y, grid_serial_number)
+      total_power_cache[x][y][1] = calculuate_power_level(x, y, grid_serial_number)
+
+  # Base case: square of side length 2
+  for x in range (1, 300):
+    for y in range(1, 300):
+      power = grid[x][y] + grid[x+1][y] + grid[x][y+1] + grid[x+1][y+1]
+      total_power_cache[x][y][2] = power 
 
   max_total_power = -90000 * 5
   max_coords = ""
 
-  square_size = 2
+  for square_size in range(3, 301):
+    for x in range(1, 300 - square_size + 2):
+      for y in range(1, 300 - square_size + 2):
 
+        # Recursive case:
+        # This square is the sum of overlapping squares of size n - 1 minus a square of n - 2
+        # plus the two non-included corners
 
-  while square_size <= 300:
-    x = 1
-    while x <= (300 - square_size + 1):
-      y = 1
-      while y <= (300 - square_size + 1):
-        total_power = total_power_cache[str(x) + "," + str(y) + "," + str(square_size-1)]
+ 
+        # Example:
+        # A square of side length 3 can be found by adding the square of size 2 with the same upper left corner
+        # with the square of side length 2 and upper left corner shifted diagonally right and down by 1 each.
+        # This captures everything but the lower left and upper right corners so we also add them in explicitly.
+        # But, the two squares of size n-1 overlap in the center so we subtract the overlap
+        # square of size n-2 to prevent double counting.
 
-        x_off = 0
-        while x_off < square_size:
-          total_power = total_power + grid[str(x + x_off) + "," + str(y + square_size - 1)]
-          x_off = x_off + 1
+        total_power = total_power_cache[x][y][square_size-1] \
+                    + total_power_cache[x+1][y+1][square_size-1] \
+                    - total_power_cache[x+1][y+1][square_size-2] \
+                    + total_power_cache[x][y+square_size-1][1] \
+                    + total_power_cache[x+square_size-1][y][1]
 
-        y_off = 0
-        while y_off < square_size:
-          total_power = total_power + grid[str(x + square_size - 1) + "," + str(y + y_off)]
-          y_off = y_off + 1
-
-        loc_key = str(x) + "," + str(y) + "," + str(square_size)
-        total_power_cache[loc_key] = total_power
+        total_power_cache[x][y][square_size] = total_power
 
         if total_power > max_total_power:
           max_total_power = total_power
-          max_coords = loc_key
-          print(max_coords, max_total_power)
-        y = y + 1
-      x = x + 1
-    square_size = square_size + 1
+          max_coords = ",".join([str(x), str(y), str(square_size)])
 
   print(max_coords)
 
